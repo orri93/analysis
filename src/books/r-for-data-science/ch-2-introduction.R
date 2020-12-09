d5f75e03d0cfce958cf9258407520373855f22e7
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-12-09 15:10:48 +0100

    Adding "R for Data Science" as sub-module

---
+++ src/books/r-for-data-science/ch-2-introduction.R
@@ -0,0 +1,6 @@
+install.packages("tidyverse")
+
+install.packages(c("nycflights13", "gapminder", "Lahman"))
+
+sessioninfo::session_info(c("tidyverse"))
+
---
+++ src/books/r-for-data-science/ch-6-workflow-scripts-61.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(nycflights13)
+
+not_cancelled <- flights %>% 
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(mean = mean(dep_delay))
+
