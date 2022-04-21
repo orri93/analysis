627eb659e70c33ac0732ed8e008679558f91eee2
Geirmundur Orri Sigurdsson <orri93@gmail.com>
2022-04-21 13:47:55 +0200

    Add a install packages script

---
+++ src/r/install-packages.R
@@ -0,0 +1,11 @@
+# ###################################################################
+#
+# Install Package dependencies
+#
+
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("data.table")
+
+# Utilities
+install.packages("foreach")
